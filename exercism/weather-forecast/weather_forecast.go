a5c04c364e252a8648fbec8902bb5792c0f7adc0
yevgeller <yevgeller@gmail.com>
2023-02-25 19:43:00 -0500

    Update on weather-forecast

--- exercism/weather-forecast/weather_forecast.go
+++ exercism/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides weather forecast.
 package weather
 
+// CurrentCondition is a variable that defines the current condition.
 var CurrentCondition string
+
+// CurrentLocation is a variable that defines the current location.
 var CurrentLocation string
 
+//
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
