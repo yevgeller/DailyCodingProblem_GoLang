a5b73a23196268617f66fdd952226330b6ba5515
yevgeller <yevgeller@gmail.com>
2023-02-25 19:46:47 -0500

    weather-forecast comments solution

--- exercism/weather-forecast/weather_forecast.go
+++ exercism/weather-forecast/weather_forecast.go
@@ -7,7 +7,7 @@ var CurrentCondition string
 // CurrentLocation is a variable that defines the current location.
 var CurrentLocation string
 
-//
+// Forecast is a function that returns the forecast.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
