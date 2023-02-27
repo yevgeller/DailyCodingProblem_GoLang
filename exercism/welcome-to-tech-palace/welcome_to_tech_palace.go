2002e2d2d1c2da94a8f689ed39e30dc63380e7f4
yevgeller <yevgeller@gmail.com>
2023-02-27 10:08:26 -0500

    welcome interim

--- exercism/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ exercism/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,8 +1,10 @@
 package techpalace
 
+import "strings"
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
