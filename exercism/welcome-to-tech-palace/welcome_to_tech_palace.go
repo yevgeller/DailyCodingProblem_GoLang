cd2a5f07e707e46559aa12abc7df4be627f2d1bb
yevgeller <yevgeller@gmail.com>
2023-02-28 10:04:26 -0500

    welcome update

--- exercism/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ exercism/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -9,7 +9,11 @@ func WelcomeMessage(customer string) string {
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	return stringBanner(numStarsPerLine) + "\n" + welcomeMsg + "\n" + stringBanner(numStarsPerLine)
+}
+
+func stringBanner(numStarsPerLine int) string {
+	return strings.Repeat("*", numStarsPerLine)
 }
 
 // CleanupMessage cleans up an old marketing message.
