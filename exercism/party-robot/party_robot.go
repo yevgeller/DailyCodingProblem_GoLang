a0465d402e8660e81dc03b9bf93884b7572e3ec9
yevgeller <yevgeller@gmail.com>
2023-03-02 09:49:15 -0500

    robot

--- exercism/party-robot/party_robot.go
+++ exercism/party-robot/party_robot.go
@@ -14,13 +14,9 @@ func HappyBirthday(name string, age int) string {
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	// Welcome to my party, Christiane!
-	// You have been assigned to table 027. Your table is on the left, exactly 23.8 meters from here.
-	// You will be sitting next to Frank.
+
 	return Welcome(name) + "\n" +
 		fmt.Sprintf("You have been assigned to table %03d. ", table) +
 		fmt.Sprintf("Your table is %s, exactly %.1f meters from here.", direction, distance) + "\n" +
 		fmt.Sprintf("You will be sitting next to %s.", neighbor)
 }
-
-//remove comments
