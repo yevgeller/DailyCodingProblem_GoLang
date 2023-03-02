0593120ae3e518237d295ce04e4d8d77a399f9dd
yevgeller <yevgeller@gmail.com>
2023-03-02 09:48:11 -0500

    robot

--- exercism/party-robot/party_robot.go
+++ exercism/party-robot/party_robot.go
@@ -17,9 +17,10 @@ func AssignTable(name string, table int, neighbor, direction string, distance fl
 	// Welcome to my party, Christiane!
 	// You have been assigned to table 027. Your table is on the left, exactly 23.8 meters from here.
 	// You will be sitting next to Frank.
-	return Welcome(name) +
-		fmt.Sprintf(" You have been assigned to table %d.", table) +
-		fmt.Sprintf(" Your table is on the %s, exactly %f.1 meters from here.", direction, distance) +
-		fmt.Sprintf(" You will be sitting next to %s.", neighbor)
-	//%.2f
+	return Welcome(name) + "\n" +
+		fmt.Sprintf("You have been assigned to table %03d. ", table) +
+		fmt.Sprintf("Your table is %s, exactly %.1f meters from here.", direction, distance) + "\n" +
+		fmt.Sprintf("You will be sitting next to %s.", neighbor)
 }
+
+//remove comments
