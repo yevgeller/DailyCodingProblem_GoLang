50d85ad566591bff198f71e1f6c13c94d1caedab
yevgeller <yevgeller@gmail.com>
2023-03-03 10:54:21 -0500

    vehicle

--- exercism/vehicle-purchase/vehicle_purchase.go
+++ exercism/vehicle-purchase/vehicle_purchase.go
@@ -1,5 +1,7 @@
 package purchase
 
+import "fmt"
+
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
 	return kind == "car" || kind == "truck"
@@ -7,7 +9,11 @@ func NeedsLicense(kind string) bool {
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	choice := option1
+if(option1 < option2) choice:= option2
+
+	
+	return fmt.Sprintf("%s is clearly the better choice", choice)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
