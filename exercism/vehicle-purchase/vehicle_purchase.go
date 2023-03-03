da8e9de15a0dee9359c227f8bb05528840aa5ef1
yevgeller <yevgeller@gmail.com>
2023-03-03 12:23:05 -0500

    vehicle

--- exercism/vehicle-purchase/vehicle_purchase.go
+++ exercism/vehicle-purchase/vehicle_purchase.go
@@ -21,5 +21,11 @@ func calculateVehicleChoice(a, b string) string {
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age < 3 {
+		return originalPrice * 0.8
+	} else if age < 10 {
+		return originalPrice * 0.7
+	} else {
+		return originalPrice * 0.5
+	}
 }
