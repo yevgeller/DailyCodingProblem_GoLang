0b8cad2e286b6586acee979127d1688296a2093e
yevgeller <yevgeller@gmail.com>
2023-03-04 15:52:40 -0500

    blackjack

--- exercism/blackjack/blackjack.go
+++ exercism/blackjack/blackjack.go
@@ -2,7 +2,42 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	/*
+	| card  | value | card    | value |
+	|  ace  |  11   | eight   |   8   |
+	|  two  |   2   | nine    |   9   |
+	| three |   3   |  ten    |  10   |
+	| four  |   4   | jack    |  10   |
+	| five  |   5   | queen   |  10   |
+	|  six  |   6   | king    |  10   |
+	| seven |   7   | *other* |   0   |
+	*/
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "jack":
+	case "queen":
+	case "king":
+		return 10
+	default:
+		return 0
+	}
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
