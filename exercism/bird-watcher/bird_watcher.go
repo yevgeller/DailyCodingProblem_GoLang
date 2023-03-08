f1a6b1d75b2fd61add8c24dc799e1f4d45aa6037
yevgeller <yevgeller@gmail.com>
2023-03-08 15:47:27 -0500

    birds

--- exercism/bird-watcher/bird_watcher.go
+++ exercism/bird-watcher/bird_watcher.go
@@ -3,17 +3,21 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	result := 0;
-	for i:=1; i < len(birdsPerDay); i++ {
-result += birdsPerDay[i]
+	result := 0
+	for i := 0; i < len(birdsPerDay); i++ {
+		result += birdsPerDay[i]
 	}
-	return result;
+	return result
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	result := 0
+	for i := week*7 - 7; i < len(birdsPerDay); i++ {
+		result += birdsPerDay[i]
+	}
+	return result
 }
 
 // FixBirdCountLog returns the bird counts after correcting
