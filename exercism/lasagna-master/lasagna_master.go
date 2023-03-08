13eb692032b53b837141c2cc9fd184a1e3aaa876
yevgeller <yevgeller@gmail.com>
2023-03-08 16:06:44 -0500

    lasagna

--- exercism/lasagna-master/lasagna_master.go
+++ exercism/lasagna-master/lasagna_master.go
@@ -1,6 +1,13 @@
 package lasagna
 
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, averagePrepTime int) int {
+	if averagePrepTime <= 0 || len(layers) == 0 {
+		return 12
+	}
+	return len(layers) * averagePrepTime
+
+}
 
 // TODO: define the 'Quantities()' function
 
