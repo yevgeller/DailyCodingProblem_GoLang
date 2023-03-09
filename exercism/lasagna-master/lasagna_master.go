ada94af9e59132fd674b33062b47e7a38be3f888
yevgeller <yevgeller@gmail.com>
2023-03-09 07:51:42 -0500

    lasa2

--- exercism/lasagna-master/lasagna_master.go
+++ exercism/lasagna-master/lasagna_master.go
@@ -10,6 +10,20 @@ func PreparationTime(layers []string, averagePrepTime int) int {
 }
 
 // TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	var grams int
+	var liters float64
+
+	for i := 0; i < len(layers); i++ {
+		if layers[i] == "noodles" {
+			grams += 50
+		}
+		if layers[i] == "sauce" {
+			liters += 0.2
+		}
+	}
+	return grams, liters
+}
 
 // TODO: define the 'AddSecretIngredient()' function
 
