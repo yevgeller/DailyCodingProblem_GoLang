f534f95d1a335063b642cb1aa36d6ba0f7f2e067
yevgeller <yevgeller@gmail.com>
2023-03-22 14:16:52 -0400

    logs

--- exercism/logs-logs-logs/logs_logs_logs.go
+++ exercism/logs-logs-logs/logs_logs_logs.go
@@ -1,21 +1,5 @@
 package logs
 
-/*
-myString := "❗hello"
-for index, char := range myString {
-  fmt.Printf("Index: %d\tCharacter: %c\t\tCode Point: %U\n", index, char, char)
-}
-
-import "unicode/utf8"
-
-myString := "❗hello"
-stringLength := len(myString)
-numberOfRunes := utf8.RuneCountInString(myString)
-
-fmt.Printf("myString - Length: %d - Runes: %d\n", stringLength, numberOfRunes)
-// Output: myString - Length: 8 - Runes: 6
-*/
-
 // Application identifies the application emitting the given log.
 func Application(log string) string {
 	for _, char := range log {
