03e5610e465f11112937a676222f7acfacb7ba7b
yevgeller <yevgeller@gmail.com>
2023-03-29 08:29:03 -0400

    airpo

--- exercism/airport-robot/airport_robot.go
+++ exercism/airport-robot/airport_robot.go
@@ -10,26 +10,26 @@ type Greeter interface {
 	Greet(name string) string
 }
 
-// func SayHello(greeter Greeter, name string) string {
-// 	return fmt.Sprintf("I can speak %s: %s", greeter.LanguageName(), greeter.Greet(name))
-// }
+func SayHello(greeter Greeter, name string) string {
+	return fmt.Sprintf("I can speak %s: %s", greeter.LanguageName(), greeter.Greet(name))
+}
 
-// type Italian struct{}
+type Italian struct{}
 
-// func (it Italian) LanguageName() string {
-// 	return "Italian"
-// }
+func (it Italian) LanguageName() string {
+	return "Italian"
+}
 
-// func (it Italian) Greet(name string) string {
-// 	return fmt.Sprintf("Ciao %s!", name)
-// }
+func (it Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
 
-// type Portuguese struct{}
+type Portuguese struct{}
 
-// func (po Portuguese) LanguageName() string {
-// 	return "Portuguese"
-// }
+func (po Portuguese) LanguageName() string {
+	return "Portuguese"
+}
 
-// func (po Portuguese) Greet(name string) string {
-// 	return fmt.Sprintf("Olá %s!", name)
-// }
+func (po Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
