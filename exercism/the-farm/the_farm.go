6ce531e2d569e08608f672eb4dd60dc199ac5bec
yevgeller <yevgeller@gmail.com>
2023-04-02 16:34:53 -0400

    the farm

---
+++ exercism/the-farm/the_farm.go
@@ -0,0 +1,71 @@
+package thefarm
+
+import (
+	"errors"
+	"fmt"
+)
+
+// See types.go for the types defined for this exercise.
+
+// TODO: Define the SillyNephewError type here.
+type SillyNephewError struct {
+	message string
+	details string
+} //errors.New("silly nephew, there cannot be -5 cows")
+
+func (e *SillyNephewError) Error() string {
+	return fmt.Sprintf("%s %s", e.message, e.details)
+}
+
+// DivideFood computes the fodder amount per cow for the given cows.
+func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
+	//panic("Please implement DivideFood")
+	//println("13")
+
+	amt, err := weightFodder.FodderAmount()
+	//println("19")
+	//println(amt, err)
+	//println("amount", amt)
+
+	//println("25")
+	// if err != nil && err.Error() == ErrScaleMalfunction.Error() && amt > 0 {
+	// 	return (amt * 2.0) / float64(cows), nil
+	// }
+
+	if err != nil && err.Error() == ErrScaleMalfunction.Error() && amt > 0 {
+		return (amt * 2.0) / float64(cows), nil
+	}
+
+
+
+	if amt < 0 && (err == nil || err.Error() == ErrScaleMalfunction.Error()) {
+		return 0.0, errors.New("negative fodder")
+	}
+	if err != nil {
+		println(err.Error(), "amt ", amt)
+		return 0.0, err
+	}
+	if cows == 0 {
+		return 0.0, errors.New("division by zero")
+	}
+	if cows < 0 {
+		//println("15")
+		return 0.0, &SillyNephewError{message: "silly nephew, there cannot be", details: fmt.Sprintf("%d cows", cows)}
+	}
+	//println("29")
+
+	//println("33")
+	// if err != nil && err.Error() == ErrScaleMalfunction.Error() && amt > 0 {
+	// 	return (amt * 2.0) / float64(cows), nil
+	// }
+
+	// if err != nil {
+	// 	println(err.Error())
+	// 	return 0.0, err
+	// }
+	// if amt < 0 {
+	// 	return 0.0, errors.New("negative fodder")
+	// }
+	//println("37")
+	return amt / float64(cows), nil
+}
