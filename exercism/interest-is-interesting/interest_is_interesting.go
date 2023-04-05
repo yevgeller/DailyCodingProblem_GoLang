d83177f4d9fd5db5b0581681f4a92ad762bc5b88
yevgeller <yevgeller@gmail.com>
2023-04-05 07:59:50 -0400

    inter

--- exercism/interest-is-interesting/interest_is_interesting.go
+++ exercism/interest-is-interesting/interest_is_interesting.go
@@ -16,7 +16,8 @@ func InterestRate(balance float64) float32 {
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	//panic("Please implement the Interest function")
+	return balance * float64(InterestRate(balance))
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
