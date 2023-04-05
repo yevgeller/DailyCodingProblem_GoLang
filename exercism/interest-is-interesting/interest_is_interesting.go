92a2368aacd8f042f6814ef4599bfba40fba7b47
yevgeller <yevgeller@gmail.com>
2023-04-05 08:00:04 -0400

    inter

--- exercism/interest-is-interesting/interest_is_interesting.go
+++ exercism/interest-is-interesting/interest_is_interesting.go
@@ -3,29 +3,40 @@ package interest
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
 	if balance < 0 {
-		return 3.213 / 100
+		return 3.213
 	}
 	if balance < 1000 {
-		return 0.5 / 100
+		return 0.5
 	}
 	if balance < 5000 {
-		return 1.621 / 100
+		return 1.621
 	}
-	return 2.475 / 100
+	return 2.475
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	//panic("Please implement the Interest function")
-	return balance * float64(InterestRate(balance))
+	return balance * float64(InterestRate(balance)) / 100
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	//panic("Please implement the AnnualBalanceUpdate function")
+	// for i := 0; i < 12; i++ {
+	// 	balance += Interest(balance)
+	// }
+
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	//panic("Please implement the YearsBeforeDesiredBalance function")
+	years := 0
+
+	for balance < targetBalance {
+		years++
+		balance += Interest(balance)
+	}
+	return years
 }
