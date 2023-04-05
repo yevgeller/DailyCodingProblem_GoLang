069bdfe3ac517a0dd643bd51ad635e081f6f4ba6
yevgeller <yevgeller@gmail.com>
2023-04-05 08:00:26 -0400

    inter

--- exercism/interest-is-interesting/interest_is_interesting.go
+++ exercism/interest-is-interesting/interest_is_interesting.go
@@ -26,7 +26,6 @@ func AnnualBalanceUpdate(balance float64) float64 {
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	//panic("Please implement the YearsBeforeDesiredBalance function")
 	years := 0
 
 	for balance < targetBalance {
