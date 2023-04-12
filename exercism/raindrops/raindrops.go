978a541db75a74a8a18c398d59beaed25fb73f3e
yevgeller <yevgeller@gmail.com>
2023-04-12 08:04:54 -0400

    raindrops

---
+++ exercism/raindrops/raindrops.go
@@ -0,0 +1,23 @@
+package raindrops
+
+import "fmt"
+
+func Convert(number int) string {
+	//panic("Please implement the Convert function")
+	var ret = ""
+	if number%3 == 0 {
+		ret += "Pling"
+	}
+	if number%5 == 0 {
+		ret += "Plang"
+	}
+	if number%7 == 0 {
+		ret += "Plong"
+	}
+
+	if len(ret) == 0 {
+		return fmt.Sprintf("%d", number)
+	}
+
+	return ret
+}
