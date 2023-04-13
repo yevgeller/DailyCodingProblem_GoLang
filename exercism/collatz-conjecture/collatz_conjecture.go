5b862acbde84e887f9603727a7e96fdc9465d5b7
yevgeller <yevgeller@gmail.com>
2023-04-13 08:05:22 -0400

    colla

---
+++ exercism/collatz-conjecture/collatz_conjecture.go
@@ -0,0 +1,23 @@
+package collatzconjecture
+
+import (
+	"errors"
+)
+
+func CollatzConjecture(n int) (int, error) {
+	//panic("Please implement the CollatzConjecture function")
+	var count = 0
+	if n <= 0 {
+		return 0, errors.New("error")
+	}
+	for n != 1 {
+		count++
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = n*3 + 1
+		}
+
+	}
+	return count, nil
+}
