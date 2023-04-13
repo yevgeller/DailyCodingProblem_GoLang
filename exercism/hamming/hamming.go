a8e11425f4be5592a1828da5834f2d7e45f37dad
yevgeller <yevgeller@gmail.com>
2023-04-13 08:06:09 -0400

    ham

--- exercism/hamming/hamming.go
+++ exercism/hamming/hamming.go
@@ -2,15 +2,15 @@ package hamming
 
 func Distance(a, b string) (int, error) {
 	//panic("Implement the Distance function")
-	//     if len(a) != len(b) {
-	//         return 0, errors.New("incomparable lengths")
-	//     }
-	// count := 0
-	//      for i, _ := range a {
-	//          if(a[i] != b[i]) {
-	//              count++
-	//          }
-	//                 //fmt.Printf("%d %c %c\n", i, str[i], elem)
-	//         }
-	// return count, nil
+	    if len(a) != len(b) {
+	        return 0, errors.New("incomparable lengths")
+	    }
+	count := 0
+	     for i, _ := range a {
+	         if(a[i] != b[i]) {
+	             count++
+	         }
+	                //fmt.Printf("%d %c %c\n", i, str[i], elem)
+	        }
+	return count, nil
 }
