d6fb17102bab8f6f9936f3af12a7966e1fbe33f4
yevgeller <yevgeller@gmail.com>
2023-04-14 13:58:41 -0400

    raindro

--- exercism/raindrops/raindrops.go
+++ exercism/raindrops/raindrops.go
@@ -1,21 +1,21 @@
 package raindrops
 
 func Convert(number int) string {
-	//panic("Please implement the Convert function")
-	// var ret = ""
-	// if number%3 == 0 {
-	// 	ret += "Pling"
-	// }
-	// if number%5 == 0 {
-	// 	ret += "Plang"
-	// }
-	// if number%7 == 0 {
-	// 	ret += "Plong"
-	// }
+	panic("Please implement the Convert function")
+	var ret = ""
+	if number%3 == 0 {
+		ret += "Pling"
+	}
+	if number%5 == 0 {
+		ret += "Plang"
+	}
+	if number%7 == 0 {
+		ret += "Plong"
+	}
 
-	// if len(ret) == 0 {
-	// 	return fmt.Sprintf("%d", number)
-	// }
+	if len(ret) == 0 {
+		return fmt.Sprintf("%d", number)
+	}
 
-	// return ret
+	return ret
 }
