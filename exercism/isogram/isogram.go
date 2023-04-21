b8814716f3570568d9f4786447cc3e0db467feed
yevgeller <yevgeller@gmail.com>
2023-04-21 13:30:59 -0400

    isogram

--- exercism/isogram/isogram.go
+++ exercism/isogram/isogram.go
@@ -1,5 +1,32 @@
 package isogram
 
+import (
+	"fmt"
+	"strings"
+)
+
+func isLetter(c rune) bool {
+	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
+}
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	//panic("Please implement the IsIsogram function")
+	m := map[string]int{}
+	fmt.Printf("Word: %s\n", word)
+	for _, r := range word {
+		//fmt.Printf("letter %s", string(r))
+		//		if isLetter(r) {
+		if ('a' <= r && r <= 'z') || ('A' <= r && r <= 'Z') {
+
+			letter := strings.ToUpper(string(r))
+
+			if m[letter] != 0 {
+				fmt.Printf("duplicate letter %s\n", letter)
+				return false
+			}
+			m[letter] = 1
+
+		}
+	}
+	return true
 }
