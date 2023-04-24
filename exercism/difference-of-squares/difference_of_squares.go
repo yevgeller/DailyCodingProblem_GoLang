45c6174a0dbd367a3e8af690a54d512a2b2f427a
yevgeller <yevgeller@gmail.com>
2023-04-23 20:05:08 -0400

    diffe

--- exercism/difference-of-squares/difference_of_squares.go
+++ exercism/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,16 @@
 package diffsquares
 
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	//panic("Please implement the SquareOfSum function")
+    return (n*(n+1)/2)*(n*(n+1)/2)
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	//panic("Please implement the SumOfSquares function")
+    return (n*(n+1)*(2*n+1))/6
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	//panic("Please implement the Difference function")
+    return SquareOfSum(n) - SumOfSquares(n)
 }
