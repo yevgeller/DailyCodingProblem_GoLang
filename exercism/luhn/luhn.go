b6c636fbda4f7cb6e7c3a7ba55f67f214a360098
yevgeller <yevgeller@gmail.com>
2023-04-25 15:30:36 -0400

    luhn

--- exercism/luhn/luhn.go
+++ exercism/luhn/luhn.go
@@ -1,5 +1,21 @@
 package luhn
 
+import "strings"
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	//panic("Please implement the Valid function")
+	for i, r := range id {
+		if i%2 == 0 {
+			if ('a' <= r && r <= 'z') || ('A' <= r && r <= 'Z') {
+
+				letter := strings.ToUpper(string(r))
+
+				if m[letter] != 0 {
+					return false
+				}
+				m[letter] = 1
+
+			}
+		}
+	}
 }
