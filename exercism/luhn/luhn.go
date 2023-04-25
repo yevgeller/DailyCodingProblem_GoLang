d4a93017f7aa3ca996caea29a2de728f741d23eb
yevgeller <yevgeller@gmail.com>
2023-04-25 15:37:59 -0400

    luhn

--- exercism/luhn/luhn.go
+++ exercism/luhn/luhn.go
@@ -16,21 +16,21 @@ if strLen %2 == 1 {
 
 for ongoingCtr >= 0 {
 	ongoingCtr -= 2
-	fmt.p
+	fmt.Println(id[ongoingCtr])
 }
 
-	for i, r := range id {
-		if i%2 == 0 {
-			if r {
+	// for i, r := range id {
+	// 	if i%2 == 0 {
+	// 		if r {
 
-				letter := strings.ToUpper(string(r))
+	// 			letter := strings.ToUpper(string(r))
 
-				if m[letter] != 0 {
-					return false
-				}
-				m[letter] = 1
+	// 			if m[letter] != 0 {
+	// 				return false
+	// 			}
+	// 			m[letter] = 1
 
-			}
-		}
-	}
+	// 		}
+	// 	}
+	// }
 }
