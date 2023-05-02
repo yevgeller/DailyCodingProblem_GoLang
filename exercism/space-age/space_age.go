fc027c780de0feede65ac6f9502629bcda57e040
yevgeller <yevgeller@gmail.com>
2023-05-02 08:21:23 -0400

    space

--- exercism/space-age/space_age.go
+++ exercism/space-age/space_age.go
@@ -1,5 +1,7 @@
 package space
 
+import ("strings")
+
 type Planet struct {
 	name string
 }
@@ -7,7 +9,7 @@ type Planet struct {
 func Age(seconds float64, planet Planet) float64 {
 	//panic("Please implement the Age function")
 
-	if planet.ToLower() == "earth" {
+	if strings.ToLower(planet.name) == "earth" {
 
 	}
 }
