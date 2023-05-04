a324bcc0a0d227c180d1cfd7c8fae497c1912da4
yevgeller <yevgeller@gmail.com>
2023-05-04 08:09:35 -0400

    resi

--- exercism/resistor-color-duo/resistor_color_duo.go
+++ exercism/resistor-color-duo/resistor_color_duo.go
@@ -4,27 +4,27 @@ package resistorcolorduo
 func Value(colors []string) int {
 	panic("Implement the Value function")
 
-	// 	m := loadMap()
-	// sum :=  m[strings.ToLower(colors[0])]*10
-	//     if(len(colors)>1) {
-	//         sum += m[strings.ToLower(colors[1])]
-	//     }
+		m := loadMap()
+	sum :=  m[strings.ToLower(colors[0])]*10
+	    if(len(colors)>1) {
+	        sum += m[strings.ToLower(colors[1])]
+	    }
 
-	//     return sum
-	// }
+	    return sum
+	}
 
-	//	func loadMap() map[string]int {
-	//		m := map[string]int{
-	//			"black":  0,
-	//			"brown":  1,
-	//			"red":    2,
-	//			"orange": 3,
-	//			"yellow": 4,
-	//			"green":  5,
-	//			"blue":   6,
-	//			"violet": 7,
-	//			"grey":   8,
-	//			"white":  9,
-	//		}
-	//		return m
+		func loadMap() map[string]int {
+			m := map[string]int{
+				"black":  0,
+				"brown":  1,
+				"red":    2,
+				"orange": 3,
+				"yellow": 4,
+				"green":  5,
+				"blue":   6,
+				"violet": 7,
+				"grey":   8,
+				"white":  9,
+			}
+			return m
 }
