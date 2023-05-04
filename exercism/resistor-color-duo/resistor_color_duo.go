07fc94af79e9f47f2c4c1a04242f5c0e2458b4a2
yevgeller <yevgeller@gmail.com>
2023-05-04 08:09:52 -0400

    resis

--- exercism/resistor-color-duo/resistor_color_duo.go
+++ exercism/resistor-color-duo/resistor_color_duo.go
@@ -1,5 +1,7 @@
 package resistorcolorduo
 
+import "strings"
+
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
 	panic("Implement the Value function")
