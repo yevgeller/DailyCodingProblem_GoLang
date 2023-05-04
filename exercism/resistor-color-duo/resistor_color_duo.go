8a4561e2dcc9a4ab8b4eaa32ace7409f2af21342
yevgeller <yevgeller@gmail.com>
2023-05-04 08:10:20 -0400

    resis

--- exercism/resistor-color-duo/resistor_color_duo.go
+++ exercism/resistor-color-duo/resistor_color_duo.go
@@ -4,29 +4,29 @@ import "strings"
 
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-	panic("Implement the Value function")
+	//panic("Implement the Value function")
 
-		m := loadMap()
-	sum :=  m[strings.ToLower(colors[0])]*10
-	    if(len(colors)>1) {
-	        sum += m[strings.ToLower(colors[1])]
-	    }
-
-	    return sum
+	m := loadMap()
+	sum := m[strings.ToLower(colors[0])] * 10
+	if len(colors) > 1 {
+		sum += m[strings.ToLower(colors[1])]
 	}
 
-		func loadMap() map[string]int {
-			m := map[string]int{
-				"black":  0,
-				"brown":  1,
-				"red":    2,
-				"orange": 3,
-				"yellow": 4,
-				"green":  5,
-				"blue":   6,
-				"violet": 7,
-				"grey":   8,
-				"white":  9,
-			}
-			return m
+	return sum
+}
+
+func loadMap() map[string]int {
+	m := map[string]int{
+		"black":  0,
+		"brown":  1,
+		"red":    2,
+		"orange": 3,
+		"yellow": 4,
+		"green":  5,
+		"blue":   6,
+		"violet": 7,
+		"grey":   8,
+		"white":  9,
+	}
+	return m
 }
