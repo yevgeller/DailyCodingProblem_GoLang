74b9f1dce46804f4d96acf800f859b4757737bbc
yevgeller <yevgeller@gmail.com>
2023-05-06 21:11:12 -0400

    nucle

--- exercism/nucleotide-count/nucleotide_count.go
+++ exercism/nucleotide-count/nucleotide_count.go
@@ -2,33 +2,34 @@ package dna
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 )
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
 // Choose a suitable data type.
-type Histogram map[string]int
+type Histogram map[rune]int
 
 // DNA is a list of nucleotides. Choose a suitable data type.
 type DNA string
 
-// Counts generates a histogram of valid nucleotides in the given DNA.
-// Returns an error if d contains an invalid nucleotide.
-///
-// Counts is a method on the DNA type. A method is a function with a special receiver argument.
-// The receiver appears in its own argument list between the func keyword and the method name.
-// Here, the Counts method has a receiver of type DNA named d.
+
 func (d DNA) Counts() (Histogram, error) {
-	flag := regexp.MustCompile(`[^ACGT]+`).MatchString(d)
+	flag := regexp.MustCompile(`[^ACGT]+`).MatchString(string(d))
 
 	if flag {
 		return nil, errors.New("Illegal character in DNA string")
 	}
 	
-	var h Histogram
-
-
-for i, c := range 
-
-	return h, nil
+results := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+	for _, nuc := range d {
+		switch nuc {
+		case 'A', 'C', 'G', 'T':
+			results[nuc]++
+		default:
+			return nil, fmt.Errorf("invalid nucleotide '%c'", nuc)
+		}
+	}
+	return results, nil
+	
 }
