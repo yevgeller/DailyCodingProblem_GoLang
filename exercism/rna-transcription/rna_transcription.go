ab0dd97849d053bf763d5c81153b4f3f51315f98
yevgeller <yevgeller@gmail.com>
2023-05-07 17:26:33 -0400

    rna

--- exercism/rna-transcription/rna_transcription.go
+++ exercism/rna-transcription/rna_transcription.go
@@ -2,4 +2,25 @@ package strand
 
 func ToRNA(dna string) string {
 	//panic("Please implement the ToRNA function")
+	ret := ""
+	for _, nuc := range dna {
+		switch nuc {
+		case 'A':
+			ret += "U"
+			break
+		case 'C':
+			ret += "G"
+			break
+		case 'G':
+			ret += "C"
+			break
+		case 'T':
+			ret += "A"
+			break
+		default:
+			break
+		}
+
+	}
+	return ret
 }
