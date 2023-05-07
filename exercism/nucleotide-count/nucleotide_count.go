a8298ade1e1ed44e1f8f4b8cdc6e0122087a3b08
yevgeller <yevgeller@gmail.com>
2023-05-06 21:11:59 -0400

    nucleo

--- exercism/nucleotide-count/nucleotide_count.go
+++ exercism/nucleotide-count/nucleotide_count.go
@@ -1,9 +1,7 @@
 package dna
 
 import (
-	"errors"
 	"fmt"
-	"regexp"
 )
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
@@ -13,15 +11,9 @@ type Histogram map[rune]int
 // DNA is a list of nucleotides. Choose a suitable data type.
 type DNA string
 
-
 func (d DNA) Counts() (Histogram, error) {
-	flag := regexp.MustCompile(`[^ACGT]+`).MatchString(string(d))
 
-	if flag {
-		return nil, errors.New("Illegal character in DNA string")
-	}
-	
-results := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+	results := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
 	for _, nuc := range d {
 		switch nuc {
 		case 'A', 'C', 'G', 'T':
@@ -31,5 +23,5 @@ results := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
 		}
 	}
 	return results, nil
-	
+
 }
