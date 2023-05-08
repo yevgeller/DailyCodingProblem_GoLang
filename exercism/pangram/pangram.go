ee83bec7d5db1dcb94495b3463443a6f8d04a973
yevgeller <yevgeller@gmail.com>
2023-05-08 09:08:43 -0400

    pangram

--- exercism/pangram/pangram.go
+++ exercism/pangram/pangram.go
@@ -1,18 +1,21 @@
 package pangram
-import ("strings"
-        "regexp")
+
+import (
+	"regexp"
+	"strings"
+)
+
 func IsPangram(input string) bool {
-	//panic("Please implement the IsPangram function")
-    input = strings.ToLower(input)
-    input = regexp.MustCompile(`[^a-z]+`).ReplaceAllString(input, "")
-//input = strings.Replace(input, " ", "", -1)
-    input = strings.ToLower(input)
-     if len(input) < 26 {
-        return false
-    }
-    var data = make(map[rune]bool)
-    for _, i := range input {
-        data[i] = true
-    }
-    return len(data) == 26
+	input = strings.ToLower(input)
+	input = regexp.MustCompile(`[^a-z]+`).ReplaceAllString(input, "")
+	//input = strings.Replace(input, " ", "", -1)
+	input = strings.ToLower(input)
+	if len(input) < 26 {
+		return false
+	}
+	var data = make(map[rune]bool)
+	for _, i := range input {
+		data[i] = true
+	}
+	return len(data) == 26
 }
