5e39d57f5ad2e32dde17e04ae5c7a62bbbf0f89d
yevgeller <yevgeller@gmail.com>
2023-05-09 08:45:32 -0400

    rever

--- exercism/reverse-string/reverse_string.go
+++ exercism/reverse-string/reverse_string.go
@@ -1,12 +1,6 @@
 package reverse
 
 func Reverse(input string) string {
-	//panic("Please implement the Reverse function")
-	//ret := ""
-	/*for i := len(input)-1; i >= 0; i-- {
-	   fmt.Println(input[i])
-	    ret += string(input[i])
-	}*/
 	res := make([]byte, len(input))
 	prevPos, resPos := 0, len(input)
 	for pos := range input {
