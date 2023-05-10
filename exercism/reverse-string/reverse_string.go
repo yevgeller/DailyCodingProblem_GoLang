d95c1e9278c6c6205c0ee9334963757d3ed29629
yevgeller <yevgeller@gmail.com>
2023-05-10 08:43:01 -0400

    rever

--- exercism/reverse-string/reverse_string.go
+++ exercism/reverse-string/reverse_string.go
@@ -3,21 +3,15 @@ package reverse
 import "fmt"
 
 func Reverse(input string) string {
-	fmt.Println("assignment: ", input)
 	result := make([]byte, len(input))
 	left, right := 0, len(input)
-	for current := range input {
-		fmt.Println("left: ", left, ", current: ", current, " right: ", right, ", result[right:]", result[right:], ", input[left:current]: ", input[left:current])
-		right -= current - left
+	for current := range input {right -= current - left
 		copy(result[right:], input[left:current])
 
 		left = current
 
 	}
-	fmt.Println("left: ", left, ", current: N/A", " right: ", right, ", result[right:]", result[right:], ", input[left:len(input)-1]: ", input[left:len(input)-1])
-	fmt.Println("result[0:]", result[0:])
 	copy(result[0:], input[left:])
-	fmt.Println()
 	return string(result)
 
 }
