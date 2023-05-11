433424f64fdafcab59ff53aff7fcd4156482acf3
yevgeller <yevgeller@gmail.com>
2023-05-11 08:36:15 -0400

    darts

--- exercism/darts/darts.go
+++ exercism/darts/darts.go
@@ -1,17 +1,19 @@
 package darts
+
 import "math"
-func Score(x, y float64) int {  //1, 5, 10
+
+func Score(x, y float64) int { //1, 5, 10
 	//panic("Please implement the Score function")
 	len := math.Sqrt(x*x + y*y)
-    //absX := math.Abs(x)
-    //absY := math.Abs(y)
+	//absX := math.Abs(x)
+	//absY := math.Abs(y)
 
-    if len <= 1 {
-        return 10
-    } else if len <= 5 {
-    return 5
-    } else if len <= 10.0 {
-    return 1
-    }
-    return 0
+	if len <= 1 {
+		return 10
+	} else if len <= 5 {
+		return 5
+	} else if len <= 10.0 {
+		return 1
+	}
+	return 0
 }
