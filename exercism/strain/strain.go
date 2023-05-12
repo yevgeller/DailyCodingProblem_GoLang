18747371645732607fab7b28b91d3fa3f18b5a4c
yevgeller <yevgeller@gmail.com>
2023-05-12 08:58:25 -0400

    strain

--- exercism/strain/strain.go
+++ exercism/strain/strain.go
@@ -5,17 +5,53 @@ type Lists [][]int
 type Strings []string
 
 func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
+	//panic("Please implement the Keep function")
+	ret := []int(nil)
+
+	for _, i := range i {
+		if filter(i) {
+			ret = append(ret, i)
+		}
+	}
+	return ret
 }
 
 func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
+	//panic("Please implement the Discard function")
+	ret := []int(nil)
+
+	for _, i := range i {
+		if !filter(i) {
+			ret = append(ret, i)
+		}
+	}
+	return ret
 }
 
 func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
+	//panic("Please implement the Keep function")
+	ret := [][]int(nil)
+
+	for _, i := range l {
+		if filter(i) {
+			ret = append(ret, i)
+		}
+	}
+	return ret
 }
 
 func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
+	//panic("Please implement the Keep function")
+	ret := []string(nil)
+
+	for _, i := range s {
+		if filter(i) {
+			ret = append(ret, i)
+		}
+	}
+	return ret
 }
+
+// func filter(int) bool {
+// 	return
+// }
