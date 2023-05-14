921134a3026e826bca33d70140b37def899e8f08
yevgeller <yevgeller@gmail.com>
2023-05-14 17:17:42 -0400

    etl

--- exercism/etl/etl.go
+++ exercism/etl/etl.go
@@ -2,6 +2,14 @@ package etl
 
 func Transform(in map[int][]string) map[string]int {
 	//panic("Please implement the Transform function")
+	var ret = make(map[string]int)
 
-	return make(map[string]int)
+	for k, v := range in {
+		// k - key, v -value
+		for a := range v {
+			ret[a] = k
+		}
+	}
+	return ret
+	//return make(map[string]int)
 }
