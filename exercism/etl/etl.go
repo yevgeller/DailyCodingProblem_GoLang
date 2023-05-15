9bcec2a9e6907128d109e0f87e47468a3e4a5e6e
yevgeller <yevgeller@gmail.com>
2023-05-15 09:32:06 -0400

    etl

--- exercism/etl/etl.go
+++ exercism/etl/etl.go
@@ -6,7 +6,6 @@ func Transform(in map[int][]string) map[string]int {
 	var ret = make(map[string]int)
 
 	for k, v := range in {
-		// k - key, v -value
 		for _, b := range v {
 			ret[strings.ToLower(b)] = k
 		}
