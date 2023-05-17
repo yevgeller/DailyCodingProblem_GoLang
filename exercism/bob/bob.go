18e835f2ccc255fecefc0894fad7705a6b8565b2
yevgeller <yevgeller@gmail.com>
2023-05-17 12:20:36 -0400

    bob

--- exercism/bob/bob.go
+++ exercism/bob/bob.go
@@ -1,15 +1,9 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package bob
 
 import (
 	"strings"
 )
 
-// Hey should have a comment documenting it.
 func Hey(remark string) string {
 	if len(strings.TrimSpace(remark)) == 0 {
 		return "Fine. Be that way!"
