351b15920ac82cdf6c595e04d918dd640d421215
yevgeller <yevgeller@gmail.com>
2023-05-18 08:34:02 -0400

    grains

--- exercism/grains/grains.go
+++ exercism/grains/grains.go
@@ -1,7 +1,10 @@
 package grains
 
+import "math"
+
 func Square(number int) (uint64, error) {
 	//panic("Please implement the Square function")
+	return uint64(math.Pow(2, number)), _
 }
 
 func Total() uint64 {
