84515fb7f994160a2c6ea1768427d9b23836ee9f
yevgeller <yevgeller@gmail.com>
2023-05-19 08:21:49 -0400

    grains

--- exercism/grains/grains.go
+++ exercism/grains/grains.go
@@ -6,14 +6,18 @@ import (
 )
 
 func Square(number int) (uint64, error) {
-	//panic("Please implement the Square function")	
+	//panic("Please implement the Square function")
 	if number <= 0 || number > 64 {
 		return 0, errors.New("bleh")
 	}
-	return uint64(math.Pow(2, float64(number))), nil
+	if number == 1 {
+		return 1, nil
+	}
+	return uint64(math.Pow(2, float64(number-1))), nil
 }
 
 func Total() uint64 {
 	//panic("Please implement the Total function")
-	return uint64(math.Pow(2, 64))
+	return math.MaxUint64
+	//return uint64(1 + math.Pow(2, 65))
 }
