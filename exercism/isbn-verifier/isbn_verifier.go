5efbc42ac2e5480ead0d661a04351b46b10de8f4
yevgeller <yevgeller@gmail.com>
2023-05-21 08:55:08 -0400

    isbn

--- exercism/isbn-verifier/isbn_verifier.go
+++ exercism/isbn-verifier/isbn_verifier.go
@@ -1,12 +1,20 @@
 package isbn
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func IsValidISBN(isbn string) bool {
 	//panic("Please implement the IsValidISBN function")
 	counter := 10
+	total := 0
 	for _, c := range isbn {
-		
+		n, err = strconv.Atoi(string(c))
+		if err == nil {
+			total += n * counter
+			counter--
+		}
 		fmt.Println(int64(c))
 	}
 
