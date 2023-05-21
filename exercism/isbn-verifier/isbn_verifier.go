42fe99b068371a61cfb1481c0a96d4df396ef865
yevgeller <yevgeller@gmail.com>
2023-05-21 08:55:14 -0400

    isbn

--- exercism/isbn-verifier/isbn_verifier.go
+++ exercism/isbn-verifier/isbn_verifier.go
@@ -2,21 +2,37 @@ package isbn
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 )
 
 func IsValidISBN(isbn string) bool {
 	//panic("Please implement the IsValidISBN function")
+	flag := regexp.MustCompile(`[0-9\-X]+`).MatchString(isbn)
+	if !flag {
+		return false
+	}
 	counter := 10
 	total := 0
+	fmt.Println("Assignment: ", isbn)
 	for _, c := range isbn {
-		n, err = strconv.Atoi(string(c))
-		if err == nil {
-			total += n * counter
+		//fmt.Println(string(c))
+
+		if s, err := strconv.Atoi(string(c)); err == nil || counter == 1 && string(c) == "X" {
+			//fmt.Printf("%T, %v", s, s)
+			if string(c) == "X" {
+				s = 10
+			}
+			total += counter * s
 			counter--
 		}
-		fmt.Println(int64(c))
+		//	i, err = strconv.ParseInt(string(c), 10, 32)
+		// if err == nil {
+		// 	total += i * counter
+		// 	counter--
+		// }
+		// fmt.Println(int64(c))
 	}
-
-	return true
+	fmt.Println("Total: ", total, " Result: ", total%11 == 0)
+	return counter == 0 && total%11 == 0
 }
