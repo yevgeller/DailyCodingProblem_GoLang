78db553f66fa33661bf5dba919c2dc44670e4d54
yevgeller <yevgeller@gmail.com>
2023-05-22 13:43:51 -0400

    isbn

--- exercism/isbn-verifier/isbn_verifier.go
+++ exercism/isbn-verifier/isbn_verifier.go
@@ -1,6 +1,7 @@
 package isbn
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -13,18 +14,22 @@ func IsValidISBN(isbn string) bool {
 	// }
 	counter := 10
 	total := 0
-	//fmt.Println("Assignment: ", isbn)
+	calc := ""
+	fmt.Println("Assignment: ", isbn)
 	for _, c := range isbn {
 		//fmt.Println(string(c))
 
-		a, b := strconv.Atoi(string(c))
-		if counter == 1 && string(c) == "X" {
+		a, err := strconv.Atoi(string(c))
+		if counter == 1 && string(c) == "X" && err != nil {
 			a = 10
+			fmt.Println("last position is X")
 		}
-		if b != nil && counter > 1 {
+		if err != nil && string(c) != "-" && counter > 1 {
+			fmt.Println("error on ", string(c), " counter: ", counter)
 			return false
 		}
-		if a > 0 {
+		if a > 0 || err == nil {
+			calc += string(c) + "*" + strconv.Itoa(counter) + " "
 			total += counter * a
 			counter--
 		}
@@ -47,6 +52,6 @@ func IsValidISBN(isbn string) bool {
 		// }
 		// fmt.Println(int64(c))
 	}
-	//fmt.Println("Total: ", total, " Result: ", total%11 == 0)
+	fmt.Println("Total: ", total, " Result: ", total%11 == 0 && counter == 0, " calc: ", calc)
 	return counter == 0 && total%11 == 0
 }
