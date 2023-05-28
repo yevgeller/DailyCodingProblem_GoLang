32ea42435deb6d695d110def34acaa805dec4547
yevgeller <yevgeller@gmail.com>
2023-05-28 16:08:40 -0400

    clock

--- exercism/clock/clock.go
+++ exercism/clock/clock.go
@@ -1,21 +1,22 @@
 package clock
 
 import "fmt"
+
 // Define the Clock type here.
 type Clock struct {
-    hour int,
-    min int
+	hour int
+	min  int
 }
 
 func New(h, m int) Clock {
-	 return {hour: h, min: min}
+	return Clock{hour: h, min: m}
 	//panic("Please implement the New function")
 }
 
 func (c Clock) Add(m int) Clock {
 	//panic("Please implement the Add function")
 	c.min += m
-	extraH = c.min / 60
+	extraH := c.min / 60
 	c.hour = (c.hour + extraH) % 24
 	c.min = c.min % 60
 }
@@ -32,6 +33,6 @@ func (c Clock) Subtract(m int) Clock {
 }
 
 func (c Clock) String() string {
-	 return fmt.PrintLn("%v:%v", c.hour, c.min)
+	return fmt.PrintLn("%v:%v", c.hour, c.min)
 	//panic("Please implement the String function")
 }
