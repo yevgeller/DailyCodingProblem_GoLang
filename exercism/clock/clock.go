ca482307a1dff9a9eac402336035d65b432d0cbc
yevgeller <yevgeller@gmail.com>
2023-05-31 11:18:16 -0400

    clock

--- exercism/clock/clock.go
+++ exercism/clock/clock.go
@@ -47,25 +47,29 @@ func initClock(h, m int) Clock {
 }
 
 func (c Clock) Add(m int) Clock {
+
+	return initClock(c.hour, c.min+m)
+
 	//panic("Please implement the Add function")
-	extraH = m / 60
-	c.min += m
-	extraH := c.min / 60
-	c.hour = (c.hour + extraH) % 24
-	c.min = c.min % 60
-	return c
+	// extraH = m / 60
+	// c.min += m
+	// extraH := c.min / 60
+	// c.hour = (c.hour + extraH) % 24
+	// c.min = c.min % 60
+	// return c
 }
 
 func (c Clock) Subtract(m int) Clock {
+	return initClock(c.hour, c.min-m)
 	//panic("Please implement the Subtract function")
-	c.min -= m
-	if c.min < 0 {
-		extraH := c.min / 60
-		c.hour -= extraH - 1
-		c.min %= 60
-		c.hour %= 24
-	}
-	return c
+	// c.min -= m
+	// if c.min < 0 {
+	// 	extraH := c.min / 60
+	// 	c.hour -= extraH - 1
+	// 	c.min %= 60
+	// 	c.hour %= 24
+	// }
+	// return c
 }
 
 func (c Clock) String() string {
