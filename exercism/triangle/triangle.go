fa967ce01619d789fe79d79f7f9911995e384d76
yevgeller <yevgeller@gmail.com>
2023-06-02 11:26:44 -0400

    trian

--- exercism/triangle/triangle.go
+++ exercism/triangle/triangle.go
@@ -5,16 +5,15 @@
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
-
 // Notice KindFromSides() returns this type. Pick a suitable data type.
 type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT = iota// not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// Pick values for the following identifiers used by the test program.
+	NaT = iota // not a triangle
+	Equ        // equilateral
+	Iso        // isosceles
+	Sca        // scalene
 )
 
 // KindFromSides should have a comment documenting it.
@@ -23,10 +22,14 @@ func KindFromSides(a, b, c float64) Kind {
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-
-	if a > (b + c) || b > (a + c) || c > (a + b) {
-		
+	if a <= 0 || b <= 0 || c <= 0 || a > (b+c) || b > (a+c) || c > (a+b){
+		return NaT
 	}
-	var k Kind
-	return k
+	if a == b && b == c {
+		return Equ
+	}
+	if (a == b || a == c || b == c) && (a + b + c) / 3 != a {
+		return Iso
+	}
+
 }
