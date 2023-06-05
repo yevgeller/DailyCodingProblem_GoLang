4a3b64c3ad003bd476392ca923803cdeb2a52e27
yevgeller <yevgeller@gmail.com>
2023-06-05 08:04:47 -0400

    prove

--- exercism/proverb/proverb.go
+++ exercism/proverb/proverb.go
@@ -4,25 +4,27 @@
 // Package proverb should have a package comment that summarizes what it's about.
 // https://golang.org/doc/effective_go.html#commentary
 package proverb
+
 import "fmt"
+
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
-    var ret []string
-	var ln := len(rhyme)
-if ln == 0 {
-    return ret
-}
-    var a := 0
-    
-    for a < (ln -1) {
-        if i < len(rhyme) {
-		ret = append(ret, fmt.Sprintf("For want of a %s the %s was lost.",  rhyme[i], rhyme[i+1]))
-		}
-    
+	var ret []string
+	ln := len(rhyme)
+	if ln == 0 {
+		return ret
+	}
+	a := 0
+
+	for a < (ln - 1) {
+		//if i < len(rhyme) {
+		ret = append(ret, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[a], rhyme[a+1]))
+		//}
+		a++
 	}
 
-    ret = append(ret, fmt.Sprintf("And all for the want of a %s", rhyme[0]))
-    return ret
+	ret = append(ret, fmt.Sprintf("And all for the want of a %s", rhyme[0])) //ADD A PERIOD HERE
+	return ret
 	// Write some code here to pass the test suite.
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
