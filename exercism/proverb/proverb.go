5b6b349070ef5410ec9ebbae9c7eeedf744f29bb
yevgeller <yevgeller@gmail.com>
2023-06-05 08:05:20 -0400

    prove

--- exercism/proverb/proverb.go
+++ exercism/proverb/proverb.go
@@ -1,13 +1,7 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package proverb
 
 import "fmt"
 
-// Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
 	var ret []string
 	ln := len(rhyme)
