4f3c02addd8db9f77a4bcd120165d35bc97729c3
yevgeller <yevgeller@gmail.com>
2023-06-06 14:16:07 -0400

    prove

--- exercism/proverb/proverb.go
+++ exercism/proverb/proverb.go
@@ -17,9 +17,4 @@ func Proverb(rhyme []string) []string {
 
 	ret = append(ret, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
 	return ret
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	//panic("Please implement the Proverb function")
 }
