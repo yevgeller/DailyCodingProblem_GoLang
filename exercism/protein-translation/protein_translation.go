d72d0593d73dda8ff8d5a0da9e1de0d48b194a63
yevgeller <yevgeller@gmail.com>
2023-06-06 14:24:28 -0400

    prote

--- exercism/protein-translation/protein_translation.go
+++ exercism/protein-translation/protein_translation.go
@@ -7,3 +7,26 @@ func FromRNA(rna string) ([]string, error) {
 func FromCodon(codon string) (string, error) {
 	panic("Please implement the FromCodon function")
 }
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	m := loadMap()
+
+	return m[strings.ToLower(color)]
+}
+
+func loadMap() map[string]int {
+	m := map[string]int{
+		"black":  0,
+		"brown":  1,
+		"red":    2,
+		"orange": 3,
+		"yellow": 4,
+		"green":  5,
+		"blue":   6,
+		"violet": 7,
+		"grey":   8,
+		"white":  9,
+	}
+	return m
+}
