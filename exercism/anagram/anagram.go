47e82c239da3d441cc7818a7e80ad6e490f3c4d3
yevgeller <yevgeller@gmail.com>
2023-06-12 10:03:10 -0400

    anagra

--- exercism/anagram/anagram.go
+++ exercism/anagram/anagram.go
@@ -3,3 +3,33 @@ package anagram
 func Detect(subject string, candidates []string) []string {
 	panic("Please implement the Detect function")
 }
+
+
+func FromCodon(codon string) (string, error) {
+	m := loadMap()
+	return m[codon], nil
+}
+
+func loadMap() map[string]string {
+	m := map[string]string{
+		"AUG": "Methionine",
+		"UUU": "Phenylalanine",
+		"UUC": "Phenylalanine",
+		"UUA": "Leucine",
+		"UUG": "Leucine",
+		"UCU": "Serine",
+		"UCC": "Serine",
+		"UCA": "Serine",
+		"UCG": "Serine",
+		"UAU": "Tyrosine",
+		"UAC": "Tyrosine",
+		"UGU": "Cysteine",
+		"UGC": "Cysteine",
+		"UGG": "Tryptophan",
+		"UAA": "STOP",
+		"UAG": "STOP",
+		"UGA": "STOP",
+	}
+	return m
+}
+
