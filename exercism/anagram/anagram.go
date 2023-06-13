21a620370de3c1c4d537444bf8b791d80f810872
yevgeller <yevgeller@gmail.com>
2023-06-13 15:09:49 -0400

    anagra

--- exercism/anagram/anagram.go
+++ exercism/anagram/anagram.go
@@ -43,20 +43,6 @@ func toMap(subject string) map[rune]int {
 // func loadMap() map[string]string {
 // 	m := map[string]string{
 // 		"AUG": "Methionine",
-// 		"UUU": "Phenylalanine",
-// 		"UUC": "Phenylalanine",
-// 		"UUA": "Leucine",
-// 		"UUG": "Leucine",
-// 		"UCU": "Serine",
-// 		"UCC": "Serine",
-// 		"UCA": "Serine",
-// 		"UCG": "Serine",
-// 		"UAU": "Tyrosine",
-// 		"UAC": "Tyrosine",
-// 		"UGU": "Cysteine",
-// 		"UGC": "Cysteine",
-// 		"UGG": "Tryptophan",
-// 		"UAA": "STOP",
 // 		"UAG": "STOP",
 // 		"UGA": "STOP",
 // 	}
