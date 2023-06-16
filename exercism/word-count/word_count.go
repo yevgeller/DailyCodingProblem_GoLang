9e2638a68ba07d9ab7551adb1e45ba912c7d589e
yevgeller <yevgeller@gmail.com>
2023-06-16 08:17:50 -0400

    wordco

--- exercism/word-count/word_count.go
+++ exercism/word-count/word_count.go
@@ -1,7 +1,49 @@
 package wordcount
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
 	//panic("Please implement the WordCount function")
+	fmt.Println("Assignment: ", phrase)
+	words := []string{}
+	word := ""
+	for _, runeValue := range phrase {
+		symbol := string(runeValue)
+		if isPunctuation(symbol) {
+			if len(word) > 0 {
+				words = append(words, strings.ToLower(word))
+			}
+			fmt.Println("Word: ", word)
+			word = ""
+		} else {
+			word += symbol
+		}
+
+	}
+	fmt.Println("Remaining word: ", word, ", len: ", len(word))
+	if len(word) > 0 {
+		words = append(words, strings.ToLower(word))
+	}
+
+	m := make(map[string]int)
+	for _, w := range words {
+		currentCount := m[w]
+		m[w] = currentCount + 1
+
+	}
+
+	return m
+}
+
+func isPunctuation(b string) bool {
+	//b := string(a)
+	result := b == "," || b == "." || b == "!" || b == ":" || b == ";" || b == "\n" || b == " "
+
+	fmt.Println("isPunctuation: ", b, " result: ", result)
+	return result
 }
