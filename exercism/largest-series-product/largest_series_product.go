703108c6a31ab441729f9627a3bff9a2bb442ba0
yevgeller <yevgeller@gmail.com>
2023-07-21 18:18:09 -0400

    largestpro

--- exercism/largest-series-product/largest_series_product.go
+++ exercism/largest-series-product/largest_series_product.go
@@ -1,5 +1,25 @@
 package lsproduct
 
+import (
+	"errors"
+	"strconv"
+)
+
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	//panic("Please implement the LargestSeriesProduct function")
+	if span < len(digits) {
+		return 0, errors.New("span must be greater or equal to string length")
+	}
+
+	converted := make([]int, len(digits))
+largestProduct := 0
+for _, c := range digits {
+digit, _ := strconv.Atoi(string(c))
+
+		//fmt.Println(i, " => ", string(c))
+		converted := append(converted, digit)
+	}
+
+
+	return 0, nil
 }
