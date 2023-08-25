7668c70c3a4725e475fb79afa58c3419e87206d1
yevgeller <yevgeller@gmail.com>
2023-08-25 08:51:31 -0400

    roman

--- exercism/roman-numerals/roman_numerals.go
+++ exercism/roman-numerals/roman_numerals.go
@@ -12,7 +12,55 @@ func ToRomanNumeral(input int) (string, error) {
 }
 
 func numberToString(input int) string {
-	if input > 1000 {
+	if input >= 1000 {
 		return "M" + numberToString(input-1000)
 	}
+	if input >= 900 {
+		return "CM" + numberToString(input-900)
+	}
+	if input > 500 {
+		return "D" + numberToString(input-500)
+	}
+
+	if input >= 400 {
+		return "CD" + numberToString(input-400)
+	}
+
+	if input >= 100 {
+		return "C" + numberToString(input-100)
+	}
+
+	if input >= 90 {
+		return "XC" + numberToString(input-90)
+	}
+
+	if input >= 50 {
+		return "L" + numberToString(input-50)
+	}
+
+	if input >= 40 {
+		return "XL" + numberToString(input-40)
+	}
+
+	if input >= 10 {
+		return "X" + numberToString(input-10)
+	}
+
+	if input >= 9 {
+		return "IX" + numberToString(input-9)
+	}
+
+	if input >= 5 {
+		return "V" + numberToString(input-5)
+	}
+
+	if input >= 4 {
+		return "IV" + numberToString(input-4)
+	}
+
+	if input >= 1 {
+		return "I" + numberToString(input-1)
+	}
+
+	return ""
 }
