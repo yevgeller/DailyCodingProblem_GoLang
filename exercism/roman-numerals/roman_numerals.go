11f51ef775d619ca3c8ef60addd3c941568c5569
yevgeller <yevgeller@gmail.com>
2023-08-25 08:40:40 -0400

    roman

--- exercism/roman-numerals/roman_numerals.go
+++ exercism/roman-numerals/roman_numerals.go
@@ -1,5 +1,11 @@
 package romannumerals
 
+import "errors"
+
 func ToRomanNumeral(input int) (string, error) {
-	panic("Please implement the ToRomanNumeral function")
+	//panic("Please implement the ToRomanNumeral function")
+if input < 1 {
+	return "", errors.New("input cannot be less than 1")
+}
+
 }
