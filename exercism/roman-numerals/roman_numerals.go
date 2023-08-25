f1b7bb39b313c366bd2530abb716caa906ff5092
yevgeller <yevgeller@gmail.com>
2023-08-25 08:43:36 -0400

    roman

--- exercism/roman-numerals/roman_numerals.go
+++ exercism/roman-numerals/roman_numerals.go
@@ -4,8 +4,15 @@ import "errors"
 
 func ToRomanNumeral(input int) (string, error) {
 	//panic("Please implement the ToRomanNumeral function")
-if input < 1 {
-	return "", errors.New("input cannot be less than 1")
+	if input < 1 {
+		return "", errors.New("input cannot be less than 1")
+	}
+
+	return numberToString(input), nil
 }
 
+func numberToString(input int) string {
+	if input > 1000 {
+		return "M" + numberToString(input-1000)
+	}
 }
