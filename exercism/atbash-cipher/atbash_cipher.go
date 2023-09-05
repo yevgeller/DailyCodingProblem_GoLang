62f85de236a1214de17c38f633b455857740eda8
yevgeller <yevgeller@gmail.com>
2023-09-05 10:55:14 -0400

    atbash

--- exercism/atbash-cipher/atbash_cipher.go
+++ exercism/atbash-cipher/atbash_cipher.go
@@ -11,27 +11,34 @@ func Atbash(s string) string {
 	normalized := strings.ToLower(s)
 	m := loadMap()
 	ret := ""
-	counter := 0
+	//counter := 0
 	word := ""
 	for _, w := range normalized {
 		if isLetter(w) {
-			counter += 1
+			//counter += 1
 			word += m[string(w)]
 			fmt.Println("word: ", word, " len(word):", len(word), " ret: ", ret, " char:", string(w))
 			//ret += m[string(w)]
 			//fmt.Println("'", string(w), "'->'", m[string(w)], "', result: ", ret, ", ret length", len(ret), " counter: ", counter)
 		} else if unicode.IsDigit(w) {
-			counter += 1
-			ret += strings.TrimSpace(string(w))
+			//counter += 1
+			//ret += strings.TrimSpace(string(w))
+			word += string(w)
+			fmt.Println("word: ", word, " len(word):", len(word), " ret: ", ret, " char:", string(w))
 			//fmt.Println("attaching number '", strings.TrimSpace(string(w)), "' to ", ret, ", ret length", len(ret), " counter: ", counter)
 		}
-		if (len(ret))%5 == 0 {
-			fmt.Println("counter: ", counter, ", attaching a space,", "symbol: ", w, ", ", string(w), " len(ret)", len(ret))
-			ret += " "
+		if len(word) == 5 {
+			ret += word + " "
+			word = ""
 		}
+
+		// if (len(ret))%5 == 0 {
+		// 	fmt.Println("counter: ", counter, ", attaching a space,", "symbol: ", w, ", ", string(w), " len(ret)", len(ret))
+		// 	ret += " "
+		// }
 		//fmt.Println("counter: ", counter, " ret:", ret, ", ret length", len(ret), ", w:", string(w))
 	}
-	return ret
+	return ret + word
 }
 func isLetter(c rune) bool {
 	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
