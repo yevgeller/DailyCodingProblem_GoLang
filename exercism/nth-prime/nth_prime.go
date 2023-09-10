7908f8a6145f4e311395c6a4a2bc596b00794c50
yevgeller <yevgeller@gmail.com>
2023-09-09 22:08:00 -0400

    nth prime

--- exercism/nth-prime/nth_prime.go
+++ exercism/nth-prime/nth_prime.go
@@ -1,13 +1,18 @@
 package prime
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
 func Nth(n int) (int, error) {
 	//panic("Please implement the Nth function")
-
+	if n < 1 {
+		return 0, errors.New("lol nice try")
+	}
 	arr := createStorageArray(n)
-	fmt.Print("the size of array is now ", len(arr))
+	fmt.Println("the size of array is now ", len(arr))
 	return 1, nil
 }
 
