69b5906789de36e827f508793a40585352e5121f
yevgeller <yevgeller@gmail.com>
2023-09-18 15:24:52 -0400

    nth_prime

--- exercism/nth-prime/nth_prime.go
+++ exercism/nth-prime/nth_prime.go
@@ -6,9 +6,7 @@ import (
 	"math"
 )
 
-// Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
 func Nth(n int) (int, error) {
-	//panic("Please implement the Nth function")
 	if n < 1 {
 		return 0, errors.New("lol nice try")
 	}
@@ -17,14 +15,14 @@ func Nth(n int) (int, error) {
 		limit = 100
 	}
 	arr := createStorageArray(int(limit))
-	limit = uint64(len(arr))
+	limit = uint64(len(arr)) - 1
 	sqrt := uint64(math.Sqrt(float64(limit)))
 	fmt.Println("the size of array is now ", len(arr), ", limit is ", limit, ", sqrt is ", sqrt)
 
 	arr[2] = true
 	arr[3] = true
 	var x, y uint64
-	//fmt.Println("sqrt is ", sqrt)
+	
 	for x = 1; x <= sqrt; x++ {
 		for y = 1; y <= sqrt; y++ {
 			a := 4*x*x + y*y
@@ -66,37 +64,6 @@ func Nth(n int) (int, error) {
 
 	}
 	fmt.Println("stopped at ", x)
-	// for (ulong x = 1; x <= sqrt; x++)
-	//     for (ulong y = 1; y <= sqrt; y++)
-	//     {
-	//         var n = 4*x*x + y*y;
-	//         if (n <= limit && (n % 12 == 1 || n % 12 == 5))
-	//             isPrime[n] ^= true;
-
-	//         n = 3*x*x + y*y;
-	//         if (n <= limit && n % 12 == 7)
-	//             isPrime[n] ^= true;
-
-	//         n = 3*x*x - y*y;
-	//         if (x > y && n <= limit && n % 12 == 11)
-	//             isPrime[n] ^= true;
-	//     }
-
-	// for (ulong n = 5; n <= sqrt; n++)
-	//     if (isPrime[n])
-
-	// ulong nSquared = n * n;
-	// for (ulong k = nSquared; k <= limit; k += nSquared)
-	//   isPrime[k] = false;
-
-	// for (ulong k = n*n; k <= limit; k *= k)
-	//     isPrime[k] = false;
-
-	// primes.Add(2);
-	// primes.Add(3);
-	// for (ulong n = 5; n <= limit; n++)
-	//     if (isPrime[n])
-	//         primes.Add(n);
 
 	return 1, nil
 }
